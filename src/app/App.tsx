07bdab80e6744d7deaab4eb9ff01d18107a3f7e4
Alexandru <alexandru.razvan2@icloud.com>
2026-05-14 16:28:26 +0300

    keep persistent cart session even if logged out

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from './routes';
-import { CartProvider } from './context/CartContext';
 
 export default function App() {
-  return (
-    <CartProvider>
-      <RouterProvider router={router} />
-    </CartProvider>
-  );
+  return <RouterProvider router={router} />;
 }
